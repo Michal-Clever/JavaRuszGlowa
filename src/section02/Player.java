371a530de459f8b3d48011ad9ca0d2580e16e302
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-04-09 19:07:50 +0200

    section02

---
+++ src/section02/Player.java
@@ -0,0 +1,16 @@
+package section02;
+
+public class Player {
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.zgaduj();
+    }
+    int liczba = 0; // tu jest zapisywana typowana liczba
+
+    public void zgaduj() {
+        liczba = (int) (Math.random() * 10);
+        System.out.println("Typuję liczbę: " + liczba);
+    }
+
+}
---
+++ src/section02/StarterGry.java
@@ -0,0 +1,8 @@
+package section02;
+
+public class StarterGry {
+    public static void main(String[] args) {
+        Zgadywanka gra = new Zgadywanka();
+        gra.rozpocznijGre();
+    }
+}
